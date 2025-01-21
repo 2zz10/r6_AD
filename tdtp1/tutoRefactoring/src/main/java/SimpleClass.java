6d49e93b3a8e3f920cbc31004db688357b0e1c48
Zaza Souleymen <szaza001@iutbayonne.univ-pau.fr>
2025-01-21 15:17:56 +0100

    first commit

---
+++ tdtp1/tutoRefactoring/src/main/java/SimpleClass.java
@@ -0,0 +1,23 @@
+public class SimpleClass {
+
+    public SimpleClass() {
+    }
+
+    /**
+     * Additionne a et b
+     * @param a - premier paramètre
+     * @param b - second paramètre
+     * @return
+     */
+    public int add(int a, int b) {
+        return a + b ;
+    }
+
+    public static void main(String[] args) {
+        new SimpleClass().add(1,2);
+
+        System.out.println("Hello world");
+    }
+}
+
+
